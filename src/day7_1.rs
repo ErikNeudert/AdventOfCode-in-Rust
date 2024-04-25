fc210cfc6b239e8d7ec565e875cf229858c1e72f
eneudert <eneudert@deloitte.com>
2024-04-25 22:50:24 +0200

    Day 7.1: draft impl for memory lookup of type
    - added mapping of 13 cards to max 5 diff hand cards, as const fn for quick lookup
    - added tests
    - had to switch u8 to usize, as u8 could not be used as index for arrays
    - switched Card enum to values from 0 - 12 inclusive, for better mapping (did not find how to get the enum index int, seems to take quite an overhead)

--- src/day7_1.rs
+++ src/day7_1.rs
@@ -4,30 +4,24 @@ use std::io::{prelude::*, BufReader};
 fn main() -> std::io::Result<()> {
     let file = File::open("res/day7_1.txt")?;
     let reader = BufReader::new(file);
-    let lines = reader.lines()
+    let hands: Vec<Hand> = reader.lines()
         .map(|line| match line {
             Ok(line) => line,
             Err(e) => panic!("Error reading line: {}", e)
-        });
-    let hands: Vec<Hand> = parse_input(Box::new(lines));
-
-    let range = calculate_winning_range(race_sheet.time, race_sheet.distance);
-    //plus one as start and end is included
-    println!("range: {:?}", range);
-    let count_winning_options = range.1 - range.0 + 1; 
-    println!(" - winning options: {}", count_winning_options);
-
-    println!("result: {}", count_winning_options);
+        })
+        .map(|line| parse_line(line))
+        .collect();
 
     Ok(())
 }
 
 struct Hand {
     typ: Typ, 
-    cards: Vec<Card>
+    cards: [usize; 5]//Vec<u8>
 }
 
 //named typ to avoid type the keyword as var name 
+#[derive(Copy, Clone, PartialEq, Eq, Debug)]
 enum Typ {
     FiveOfAKind,
     FourOfAKind,
@@ -38,68 +32,188 @@ enum Typ {
     HighCard
 }
 
+
+const TYP_MATRIX: [[[[[Typ; 5]; 5]; 5]; 5]; 5] = initialize_typ_matrix();
 //lets go memory brute force
-impl From<Vec<Card>> for Typ {
-    fn from(cards: Vec<Card>) -> Self {
-        /* Question: How can I identify types with least card comparisos?
-        Brute force: 
-            nr accesses: 4 + 3 + 2 + 1 = 10
-            memory: 3bit counter * 5 cards = 15bit
-            compare card 1 to 2-5, note 
-            
-        Memory brute force: 
-            nr accesses: 1
-            memory: 55dim array, 5^5=3125 * (5 cards + 1 Typ)
-                all possibilities in a matrix of theoretical size:
-                    4bit required for 13 card enum possiblities,
-                    3bit for 7 Typ possibilities
-                    5cards*4bit + 1type*3bit = 23bit
-                    3125matrix points * 23bit = 9375+62500 =71875bit ~ 71kb
-         */
-    }
-}
-
-enum Card {
-    A, K, Q, J, T, 9, 8, 7, 6, 5, 4, 3, 2 //if uncompilable, use x9 or c9 for numbers
-}
-
-impl From<char> for Card {
-    fn from(char: char) -> Self {
-        match char {
-            'A' => A,
-            'K' => K,
-            'Q' => Q,
-            'J' => J,
-            'T' => T,
-            '9' => 9,
-            '8' => 8,
-            '7' => 7,
-            '6' => 6,
-            '5' => 5,
-            '4' => 4,
-            '3' => 3,
-            '2' => 2
-            _ => panic!("Unhandled Card char: {}", char)
+//no init time, due to const fn!! calculate at compile time
+const fn initialize_typ_matrix() -> [[[[[Typ; 5]; 5]; 5]; 5]; 5] {
+    let mut typ_matrix = [[[[[Typ::HighCard; 5]; 5]; 5]; 5]; 5];
+    
+    let mut i0: usize = 0;
+    while i0 <= 4 {
+        let mut i1: usize = 0;
+        while i1 <= 4 {
+            let mut i2: usize = 0;
+            while i2 <= 4 {
+                let mut i3: usize = 0;
+                while i3 <= 4 {
+                    let mut i4: usize = 0;
+                    while i4 <= 4 {
+                        //this is basically the same approach as just calculating the hand on the fly xD
+                        let typ = identify_hand_type([i0, i1, i2, i3, i4]);
+                        typ_matrix[i0][i1][i2][i3][i4] = typ;                        
+                        i4 += 1;
+                    }
+                    i3 += 1;
+                }
+                i2 += 1;
+            }
+            i1 += 1;
         }
+        i0 += 1;
+    }
+    // for i0 in 0..4 {
+    //     for i1 in 0..4 {
+    //         for i2 in 0..4 {
+    //             for i3 in 0..4 {
+    //                 for i4 in 0..4 {
+    //                 }
+    //             }
+    //         }
+    //     }
+    // }
+
+    typ_matrix
+}
+
+//identify type based on the card similarity
+const fn identify_hand_type(cards: [usize; 5]) -> Typ {
+    let mut occurrences = [0; 5];
+    let mut i = 0;
+    while i < cards.len() {
+        let card1 = cards[i];
+        let mut ri = i;
+        while ri < cards.len() {
+            let card2 = cards[ri];
+            if card1 == card2 {
+                occurrences[i] += 1;
+            }
+            ri += 1;
+        }
+        i += 1;
+    }
+
+
+    let mut has_pair = false;
+    let mut has_three = false;
+    let mut i = 0;
+    while i < occurrences.len() {
+        let occurrence = occurrences[i];
+        if occurrence == 5 {
+            return Typ::FiveOfAKind;
+        }
+        if occurrence == 4 {
+            return Typ::FourOfAKind;
+        }
+        if occurrence == 3 {
+            if has_pair {
+                return Typ::FullHouse;
+            } else {
+                has_three = true;
+            }
+        }
+        if occurrence == 2 {
+            if has_pair {
+                return Typ::TwoPair;
+            } else if has_three {
+                return Typ::FullHouse;
+            } else {
+                has_pair = true;
+            }
+        }
+        i += 1;
+    }
+    if has_three {
+        return Typ::ThreeOfAKind;
+    } else if has_pair {
+        return Typ::OnePair;
+    }
+    return Typ::HighCard;
+}
+
+
+// impl From<Vec<u8>> for Typ {
+//     fn from(cards: Vec<u8>) -> Self {
+//         //reduce to max 5 options:
+//         let mut option_counter = 0;
+//         let mut option_map: [u8; 13] /*array type*/ = [8 as u8; 13];
+//         let hand_pattern: Vec<u8> = cards.into_iter()
+//             .map(|c| to_card(c))
+//             .map(|c| {
+//                 let mut option = option_map[c];
+//                 if option == 8 { 
+//                     //only 5 values (0-4), 8 means uninitialized (only 1 bit flip from 0 -> 0000_1000 :-)
+//                     option = option_counter;
+//                     option_map[c] = option_counter;
+//                     option_counter += 1;
+//                 }
+//                 option
+//             })
+//             .collect::<Vec<u8>>();
+//         //start with number 5
+//         //for each card, compare with next card.
+//             //array of 13 elements:
+//             //for each card, access the array at it's index. 0-12
+//             //if the element doesnt exist, increment a counter and put the number at the index
+//             //map the card to the counter
+
+//         /* Question: How can I identify types with least card comparisos?
+//         Brute force: 
+//             nr accesses: 4 + 3 + 2 + 1 = 10
+//             memory: 3bit counter * 5 cards = 15bit
+//             compare card 1 to 2-5, note 
+            
+//         Memory brute force: 
+//             nr accesses: 1
+//             memory: 55dim array, 5^5=3125 * (5 cards + 1 Typ)
+//                 all possibilities in a matrix of theoretical size:
+//                     4bit required for 13 card enum possiblities,
+//                     3bit for 7 Typ possibilities
+//                     5cards*4bit + 1type*3bit = 23bit
+//                     3125matrix points * 23bit = 9375+62500 =71875bit ~ 71kb
+
+//             5^5 is only possible, if I find a way to shrink the 13 card possibilities to a range of 5
+//          */
+//     }
+// }
+
+fn to_card(char: char) -> usize {
+    match char {
+        'A' => 12,
+        'K' => 11,
+        'Q' => 10,
+        'J' => 9,
+        'T' => 8,
+        '9' => 7,
+        '8' => 6,
+        '7' => 5,
+        '6' => 4,
+        '5' => 3,
+        '4' => 2,
+        '3' => 1,
+        '2' => 0,
+        _ => panic!("Unhandled Card char: {}", char)
     }
 }
 
 // 32T3K 765
 fn parse_line(line: String) -> Hand {
     let (hand, bid) = match line.split_once(' ') {
-        Some(tuple) => tuple
+        Some(tuple) => tuple,
         None => panic!("line should contain exactly one blank space")
     };
 
-    let cards: Vec<Card> = hand.chars()
-        .map(|c| Card::from(c))
-        .collect();
+    let cards: [usize; 5] = hand.chars()
+        .map(|c| to_card(c))
+        .collect::<Vec<usize>>()
+        .try_into()
+        .unwrap();
 
+    let typ = identify_hand_type(cards);
     //type from cards
-
-    RaceSheet {
-        time: time,
-        distance: distance
+    Hand {
+        typ: typ,
+        cards: cards
     }
 }
 
@@ -108,10 +222,34 @@ mod tests {
     use crate::*;
 
     #[test]
+    fn test_initialize_typ_matrix() {
+        let typ_matrix = initialize_typ_matrix();
+
+        assert_eq!(Typ::FiveOfAKind, typ_matrix[0][0][0][0][0]);
+        assert_eq!(Typ::FourOfAKind, typ_matrix[0][0][0][0][1]);
+        assert_eq!(Typ::FullHouse, typ_matrix[0][0][0][1][1]);
+        assert_eq!(Typ::ThreeOfAKind, typ_matrix[0][0][0][1][2]);
+        assert_eq!(Typ::TwoPair, typ_matrix[0][0][1][1][2]);
+        assert_eq!(Typ::OnePair, typ_matrix[0][0][1][2][3]);
+        assert_eq!(Typ::HighCard, typ_matrix[0][1][2][3][4]);
+    }
+    #[test]
+    fn test_identify_hand_type() {
+        assert_eq!(Typ::FiveOfAKind, identify_hand_type([0, 0, 0, 0, 0]));
+        assert_eq!(Typ::FourOfAKind, identify_hand_type([0, 0, 0, 0, 1]));
+        assert_eq!(Typ::FullHouse, identify_hand_type([0, 0, 0, 1, 1]));
+        assert_eq!(Typ::ThreeOfAKind, identify_hand_type([0, 0, 0, 1, 2]));
+        assert_eq!(Typ::TwoPair, identify_hand_type([0, 0, 1, 1, 2]));
+        assert_eq!(Typ::OnePair, identify_hand_type([0, 0, 1, 2, 3]));
+        assert_eq!(Typ::HighCard, identify_hand_type([0, 1, 2, 3, 4]));
+    }
+
+    #[test]
     fn test_parse_line() {
         let input = "32T3K 765";
-        let hand: Hand = parse_line(input);
+        let hand: Hand = parse_line(input.to_string());
 
-        assert_eq!(71530, sheet.time);
+        assert_eq!(Typ::OnePair, hand.typ);
+        assert_eq!([1, 0, 8, 1, 11], hand.cards);
     }
 }
